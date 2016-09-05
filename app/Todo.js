2b49a6871a1506fb33b2d4faa51551899dd4b0de
Anulfo <noaharkling@gmail.com>
2016-09-05 11:06:10 -0500

    One and two way binding

--- app/Todo.js
+++ app/Todo.js
@@ -1,5 +1,16 @@
+"use strict";
+
 var app = angular.module("TodoApp", []);
 
 app.controller("TodoCtrl", function($scope) {
     $scope.title = "Welcome to your task list";
+    $scope.macaroni = "";
+
+    $scope.todos = [
+    {name: "mow the lawn", complete: "incomplete"},
+    {name: "cut the grass", complete: "incomplete"},
+    {name: "kill the ants", complete: "incomplete"},
+    {name: "trimthe weeds", complete: "complete"},
+    ]
 });
+
