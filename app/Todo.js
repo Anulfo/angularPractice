e9bb2ee2d9e78f899343eb19b687752e3b8a64ab
Anulfo <noaharkling@gmail.com>
2016-09-06 09:00:52 -0500

    few changes

--- app/Todo.js
+++ app/Todo.js
@@ -11,6 +11,14 @@ app.controller("TodoCtrl", function($scope) {
     {name: "cut the grass", complete: "incomplete"},
     {name: "kill the ants", complete: "incomplete"},
     {name: "trimthe weeds", complete: "complete"},
-    ]
+    ];
+
+    $scope.killTodo = function (currentTask) {
+        var currentTaskIndex = $scope.todos.indexOf(currentTask);
+        console.log(currentTaskIndex);
+        if (currentTaskIndex >= 0) {
+            $scope.todos.splice(currentTaskIndex, 1);
+        }
+    };
 });
 
